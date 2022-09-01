16251e64db0c4df853c06e08c8bbf3ccc55c5076
Damie Vongsy-Kaneshige <Damievk808@gmail.com>
2022-09-01 16:10:00 -0700

    changed-js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('hello World!');
+console.log('Hello World!');
+
+let usersLocation = prompt('where are you from?');
+
+let message;
+
+if (usersLocation == 'Hawaii'){
+    message = 'Lucky we live HI';
+} else if (usersLocation == 'California'){
+    message = '=( I am sorry';
+} else {
+    message = 'Awesome enjoy your visit!'
+}
+document.write(message)
+
