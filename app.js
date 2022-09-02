f6b1fa999f07a2aeeac88fa940e96f12bb4aabc6
Damie Vongsy-Kaneshige <Damievk808@gmail.com>
2022-09-01 22:36:48 -0700

    addedquiz

--- app.js
+++ app.js
@@ -2,17 +2,29 @@
 
 console.log('Hello World!');
 
-let usersLocation = prompt('where are you from?');
 
-let message;
 
-if (usersLocation == 'Hawaii'){
-    message = 'Lucky we live HI';
-} else if (usersLocation == 'California'){
-    message = '=( I am sorry';
-} else {
-    message = 'Awesome enjoy your visit!'
+function usersLocation(){
+    let message
+    let usersLocation = prompt('where are you from?');
+    if (usersLocation == 'Hawaii'){
+        message = 'Lucky we live HI';
+    } else if (usersLocation == 'California'){
+        message = '=( I am sorry';
+    } else {
+        message = 'Awesome enjoy your visit!'
+    }
+    document.write(message)  
 }
-document.write(message)
 
-function 
+usersLocation(); 
+
+function quizQuestion() {
+    let quizans = prompt('What time zone is Hawaii in?')
+    while (quizans != 'HST') {
+        quizans = prompt('wrong')
+    } if (quizans === 'PST') {
+        alert('Great Job!');
+
+    }
+}
